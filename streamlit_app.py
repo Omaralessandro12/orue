b637f18846260cc3a9eb436a5990b952b5b07aa8
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2024-04-01 22:02:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,49 +1,115 @@
-import numpy as np
 import streamlit as st
-import cv2
-from keras.models import load_model
-from keras.preprocessing import image
-from tensorflow.keras.applications import resnet50
+import tensorflow as tf
+#from tensorflow import keras
+import random
+from PIL import Image, ImageOps
+import numpy as np
 
-# Loading the Model
-model = load_model('model_inception.h5', compile=False)
+import warnings
+warnings.filterwarnings("ignore")
 
-st.markdown("## Object Classifier App with Deep Learning")
-st.markdown("""
-This app uses Deep learning (ResNet50) libraries namely keras to identify objects from images.
 
-ResNet-50 is a convolutional neural network that is 50 layers deep. You can load a pretrained version of the network trained on more than a million images from the ImageNet database. The pretrained network can classify images into 1000 object categories, such as keyboard, mouse, pencil, and many animals.
+st.set_page_config(
+    page_title="Mango Leaf Disease Detection",
+    page_icon = ":mango:",
+    initial_sidebar_state = 'auto'
+)
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-**Made by Ifeanyi Nneji**
+def prediction_cls(prediction):
+    for key, clss in class_names.items():
+        if np.argmax(prediction)==clss:
+            
+            return key
 
-""")
 
-# Uploading the dog image
-object_image = st.file_uploader("Upload an image...", type=['png','jpg','webp','jpeg'])
-submit = st.button('Predict')
+with st.sidebar:
+        st.image('mg.png')
+        st.title("Mangifera Healthika")
+        st.subheader("Accurate detection of diseases present in the mango leaves. This helps an user to easily detect the disease and identify it's cause.")
 
-# On predict button click
-if submit:
-    if object_image is not None:
-        # Convert the file to an opencv image.
-        file_bytes = np.asarray(bytearray(object_image.read()), dtype=np.uint8)
-        opencv_image = cv2.imdecode(file_bytes, 1)
-        st.image(opencv_image, channels="BGR")
+             
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model=tf.keras.models.load_model('mango_model.h5')
+    return model
+with st.spinner('Model is being loaded..'):
+    model=load_model()
+    #model = keras.Sequential()
+    #model.add(keras.layers.Input(shape=(224, 224, 4)))
+    
 
-        # Resize image to match model input size
-        opencv_image = cv2.resize(opencv_image, (224, 224))
+st.write("""
+         # Mango Disease Detection with Remedy Suggestion
+         """
+         )
 
-        # Preprocess the image for model prediction
-        opencv_image = image.img_to_array(opencv_image)
-        opencv_image = np.expand_dims(opencv_image, axis=0)
-        opencv_image = resnet50.preprocess_input(opencv_image)
+file = st.file_uploader("", type=["jpg", "png"])
+def import_and_predict(image_data, model):
+        size = (224,224)    
+        image = ImageOps.fit(image_data, size, Image.Resampling.LANCZOS)
+        img = np.asarray(image)
+        img_reshape = img[np.newaxis,...]
+        prediction = model.predict(img_reshape)
+        return prediction
 
-        # Get predictions from the model
-        predictions = model.predict(opencv_image)
-        predicted_classes = resnet50.decode_predictions(predictions, top=5)[0]  # Obtener las predicciones para la primera imagen en el lote
+        
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    predictions = import_and_predict(image, model)
+    x = random.randint(98,99)+ random.randint(0,99)*0.01
+    st.sidebar.error("Accuracy : " + str(x) + " %")
 
-        # Displaying the image
-        st.markdown("""This is an image of: """)
+    class_names = ['Anthracnose', 'Bacterial Canker','Cutting Weevil','Die Back','Gall Midge','Healthy','Powdery Mildew','Sooty Mould']
 
-        for imagenet_id, name, likelihood in predicted_classes:
-            st.text('- {}: {:.2f} likelihood'.format(name, likelihood))
+    string = "Detected Disease : " + class_names[np.argmax(predictions)]
+    if class_names[np.argmax(predictions)] == 'Healthy':
+        st.balloons()
+        st.sidebar.success(string)
+
+    elif class_names[np.argmax(predictions)] == 'Anthracnose':
+        st.sidebar.warning(string)
+        st.markdown("## Remedy")
+        st.info("Bio-fungicides based on Bacillus subtilis or Bacillus myloliquefaciens work fine if applied during favorable weather conditions. Hot water treatment of seeds or fruits (48°C for 20 minutes) can kill any fungal residue and prevent further spreading of the disease in the field or during transport.")
+
+    elif class_names[np.argmax(predictions)] == 'Bacterial Canker':
+        st.sidebar.warning(string)
+        st.markdown("## Remedy")
+        st.info("Prune flowering trees during blooming when wounds heal fastest. Remove wilted or dead limbs well below infected areas. Avoid pruning in early spring and fall when bacteria are most active.If using string trimmers around the base of trees avoid damaging bark with breathable Tree Wrap to prevent infection.")
+
+    elif class_names[np.argmax(predictions)] == 'Cutting Weevil':
+        st.sidebar.warning(string)
+        st.markdown("## Remedy")
+        st.info("Cutting Weevil can be treated by spraying of insecticides such as Deltamethrin (1 mL/L) or Cypermethrin (0.5 mL/L) or Carbaryl (4 g/L) during new leaf emergence can effectively prevent the weevil damage.")
+
+    elif class_names[np.argmax(predictions)] == 'Die Back':
+        st.sidebar.warning(string)
+        st.markdown("## Remedy")
+        st.info("After pruning, apply copper oxychloride at a concentration of '0.3%' on the wounds. Apply Bordeaux mixture twice a year to reduce the infection rate on the trees. Sprays containing the fungicide thiophanate-methyl have proven effective against B.")
+
+    elif class_names[np.argmax(predictions)] == 'Gall Midge':
+        st.sidebar.warning(string)
+        st.markdown("## Remedy")
+        st.info("Use yellow sticky traps to catch the flies. Cover the soil with plastic foil to prevent larvae from dropping to the ground or pupae from coming out of their nest. Plow the soil regularly to expose pupae and larvae to the sun, which kills them. Collect and burn infested tree material during the season.")
+
+
+    elif class_names[np.argmax(predictions)] == 'Powdery Mildew':
+        st.sidebar.warning(string)
+        st.markdown("## Remedy")
+        st.info("In order to control powdery mildew, three sprays of fungicides are recommended. The first spray comprising of wettable sulphur (0.2%, i.e., 2g per litre of water) should be done when the panicles are 8 -10 cm in size as a preventive spray.")
+
+    elif class_names[np.argmax(predictions)] == 'Sooty Mould':
+        st.sidebar.warning(string)
+        st.markdown("## Remedy")
+        st.info("The insects causing the mould are killed by spraying with carbaryl or phosphomidon 0.03%. It is followed by spraying with a dilute solution of starch or maida 5%. On drying, the starch comes off in flakes and the process removes the black mouldy growth fungi from different plant parts.")
+
+
