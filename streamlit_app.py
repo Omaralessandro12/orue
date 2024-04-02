b8cbf6d2037643e3f727a6b04aad58aa11aa375d
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2024-04-01 21:54:03 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,12 @@
-#Library imports
 import numpy as np
 import streamlit as st
 import cv2
 from keras.models import load_model
-#import joblib
 from keras.preprocessing import image
-from io import BytesIO
 from tensorflow.keras.applications import resnet50
 
-
-
-#Loading the Model
+# Loading the Model
 model = load_model('model_inception.h5', compile=False)
-
 
 st.markdown("## Object Classifier App with Deep Learning")
 st.markdown("""
@@ -24,31 +18,32 @@ ResNet-50 is a convolutional neural network that is 50 layers deep. You can load
 
 """)
 
-#Name of Classes
-
-
-#Uploading the dog image
+# Uploading the dog image
 object_image = st.file_uploader("Upload an image...", type=['png','jpg','webp','jpeg'])
 submit = st.button('Predict')
-#On predict button click
+
+# On predict button click
 if submit:
-
     if object_image is not None:
-
         # Convert the file to an opencv image.
         file_bytes = np.asarray(bytearray(object_image.read()), dtype=np.uint8)
         opencv_image = cv2.imdecode(file_bytes, 1)
         st.image(opencv_image, channels="BGR")
-        opencv_image = cv2.resize(opencv_image, (224,224))
-        opencv_image.shape = (1,224,224,3)
+
+        # Resize image to match model input size
+        opencv_image = cv2.resize(opencv_image, (224, 224))
+
+        # Preprocess the image for model prediction
+        opencv_image = image.img_to_array(opencv_image)
+        opencv_image = np.expand_dims(opencv_image, axis=0)
+        opencv_image = resnet50.preprocess_input(opencv_image)
+
+        # Get predictions from the model
         predictions = model.predict(opencv_image)
         predicted_classes = resnet50.decode_predictions(predictions, top=5)
 
-
-
         # Displaying the image
-        #st.image(object_image, channels="BGR")
         st.markdown("""This is an image of: """)
 
         for imagenet_id, name, likelihood in predicted_classes[0]:
-            st.text('- {}: {:.2f} likelihood'.format(name,likelihood))
+            st.text('- {}: {:.2f} likelihood'.format(name, likelihood))
