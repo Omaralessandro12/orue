a57847dc90902157ed0260808c3878161b2b04f9
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2024-04-14 10:36:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,59 @@
-import streamlit as st
-import tensorflow as tf
-from PIL import Image, ImageOps
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.imagenet_utils import preprocess_input
+
 import numpy as np
-import random
 
-import warnings
-warnings.filterwarnings("ignore")
 
-st.set_page_config(
-    page_title="Mango Leaf Disease Detection",
-    page_icon=":mango:",
-    initial_sidebar_state='auto'
-)
+import streamlit as st
+from PIL import Image
+from skimage.transform import resize
 
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-@st.cache(allow_output_mutation=True)
-def load_model():
-    model = tf.keras.models.load_model('models_resnet50.h5')
-    return model
+# Path del modelo preentrenado
+MODEL_PATH = 'models/model_MobileNet_Covid.h5'
 
-def prediction_cls(prediction):
-    class_names = ['Anthracnose', 'Bacterial Canker', 'Cutting Weevil', 'Die Back', 'Gall Midge', 'Healthy', 'Powdery Mildew', 'Sooty Mould']
-    return class_names[np.argmax(prediction)]
+# Dimensiones de las imagenes de entrada    
+width_shape = 224
+height_shape = 224
 
-st.write("# Prueba Resnet50")
+# Clases
+names = ['NoCovid','Covid']
 
-file = st.file_uploader("", type=["jpg", "png"])
 
-def import_and_predict(image_data, model):
-    class_names = ['Anthracnose', 'Bacterial Canker', 'Cutting Weevil', 'Die Back', 'Gall Midge', 'Healthy', 'Powdery Mildew', 'Sooty Mould']
-    size = (224, 224)
-    image = ImageOps.fit(image_data, size, Image.Resampling.LANCZOS)
-    img = np.asarray(image)
-    img_reshape = img[np.newaxis,...]
-    prediction = model.predict(img_reshape)
-    predicted_class = prediction_cls(prediction)
-    return predicted_class, prediction
+# Se recibe la imagen y el modelo, devuelve la predicción
+def model_prediction(img, model):
 
-if file is None:
-    st.text("Please upload an image file")
-else:
-    model = load_model()
-    image = Image.open(file)
-    st.image(image, use_column_width=True)
-    predicted_class, predictions = import_and_predict(image, model)
-    accuracy = random.randint(98, 99) + random.randint(0, 99) * 0.01
-    st.sidebar.error("Accuracy : " + str(accuracy) + " %")
-    st.sidebar.success("Detected Disease : " + predicted_class)
+    img_resize = resize(img, (width_shape, height_shape))
+    x=preprocess_input(img_resize*255)
+    x = np.expand_dims(x,axis=0)
+    
+    preds = model.predict(x)
+    return preds
+
+
+def main():
+    
+    model=''
+
+    # Se carga el modelo
+    if model=='':
+        model = load_model(MODEL_PATH)
+    
+    st.title("CLASIFICADOR PACIENTES COVID IMAGENES CT")
+
+    predictS=""
+    img_file_buffer = st.file_uploader("Carga una imagen", type=["png", "jpg", "jpeg"])
+    
+    # El usuario carga una imagen
+    if img_file_buffer is not None:
+        image = np.array(Image.open(img_file_buffer))    
+        st.image(image, caption="Imagen", use_column_width=False)
+    
+    # El botón predicción se usa para iniciar el procesamiento
+    if st.button("Predicción"):
+         predictS = model_prediction(image, model)
+         st.success('EL DIAGNÓSTICO ES: {}'.format(names[np.argmax(predictS)]))
+
+
+if __name__ == '__main__':
+    main()
