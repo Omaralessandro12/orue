cf811699b3dab32ff2365ae39bb0cc19cd2ac0d9
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2024-04-21 09:33:18 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,52 +1,45 @@
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.imagenet_utils import preprocess_input
+import numpy as np
+
 import streamlit as st
 from PIL import Image
-import numpy as np
-from tensorflow.keras.models import model_from_json
-import io
+from skimage.transform import resize
 
-# Clases de salida del modelo VGG16
+# Path del modelo preentrenado
+MODEL_PATH = 'models_resnet50.h5'
+
+width_shape = 224
+height_shape = 224
+
 names = ['ARAÑA ROJA', 'MOSCA BLANCA', 'MOSCA FRUTA', 'PULGON VERDE', 'PICUDO ROJO']
 
-def load_and_preprocess_image(img):
-    img = img.resize((224, 224))
-    img = np.array(img)
-    img = np.expand_dims(img, axis=0)
-    img = img.astype('float32')
-    img = img / 255.0
-    return img
-
 def model_prediction(img, model):
-    img = load_and_preprocess_image(img)
-    preds = model.predict(img)
+    img_resize = resize(img, (width_shape, height_shape))
+    x = preprocess_input(img_resize * 255)
+    x = np.expand_dims(x, axis=0)
+    
+    preds = model.predict(x)
     return preds
 
 def main():
     model = None
 
-    # Cargar arquitectura del modelo
-    with open('modelo_vgg16.json', 'r') as json_file:
-        loaded_model_json = json_file.read()
-    model = model_from_json(loaded_model_json)
-
-    # Cargar pesos del modelo
-    model.load_weights('/ruta/completa/a/vgg16_weights_tf_dim_ordering_tf_kernels.h5')
-
+    if model is None:
+        model = load_model(MODEL_PATH)
+    
     st.title("bichos :sunglasses:")
     
     predictS = ""
     img_file_buffer = st.file_uploader("Carga una imagen", type=["png", "jpg", "jpeg"])
     
     if img_file_buffer is not None:
-        # Convertir buffer de archivo a objeto de imagen
-        img = Image.open(io.BytesIO(img_file_buffer.read()))
-        st.image(img, caption="Imagen", use_column_width=False)
+        image = np.array(Image.open(img_file_buffer))    
+        st.image(image, caption="Imagen", use_column_width=False)
     
     if st.button("Predicción"):
-        if model is not None:
-            predictS = model_prediction(img, model)
-            st.success('LA CLASE ES: {}'.format(names[np.argmax(predictS)]))
-        else:
-            st.error('El modelo no se ha cargado correctamente. Verifica que los archivos del modelo estén en la ubicación correcta.')
+         predictS = model_prediction(image, model)
+         st.success('LA CLASE ES: {}'.format(names[np.argmax(predictS)]))
 
 if __name__ == '__main__':
     main()
